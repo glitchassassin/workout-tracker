102d040a9122eaab2f347d6da805f3ef9b0127cb
Jon Winsley <jon.winsley@gmail.com>
2025-07-02 13:16:14 -0400

    autofocus reps

--- script.js
+++ script.js
@@ -379,6 +379,15 @@ class WorkoutTracker {
         this.renderWeekList();
         this.renderWorkoutTabs();
         this.renderWorkoutContent();
+        
+        // Focus the first field (reps/seconds/minutes) of the newly added set
+        setTimeout(() => {
+            const newSetIndex = workoutData.exercises[exerciseName].sets.length - 1;
+            const firstInput = document.querySelector(`[data-exercise="${exerciseName}"][data-set="${newSetIndex}"][data-field="reps"], [data-exercise="${exerciseName}"][data-set="${newSetIndex}"][data-field="seconds"], [data-exercise="${exerciseName}"][data-set="${newSetIndex}"][data-field="minutes"]`);
+            if (firstInput) {
+                firstInput.focus();
+            }
+        }, 0);
     }
 
     removeSet(exerciseName, setIndex) {
